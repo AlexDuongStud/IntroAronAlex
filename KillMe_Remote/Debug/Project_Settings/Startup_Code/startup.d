a7749625609c6f8cc0ba396b930f9d46d6c15853
AlexDuongStud <alex.duong@stud.hslu.ch>
2018-09-21 15:47:16 +0200

    Restart

--- KillMe_Remote/Debug/Project_Settings/Startup_Code/startup.d
+++
@@ -1,8 +0,0 @@
-Project_Settings/Startup_Code/startup.o: \
- ../Project_Settings/Startup_Code/startup.c \
- /Users/alexduong/Desktop/HSLU/HS_18/INTRO/workspace.kds/KillMe_Remote/Generated_Code/IO_Map.h \
- /Users/alexduong/Desktop/HSLU/HS_18/INTRO/workspace.kds/KillMe_Remote/Static_Code/IO_Map/MK20D5.h
-
-/Users/alexduong/Desktop/HSLU/HS_18/INTRO/workspace.kds/KillMe_Remote/Generated_Code/IO_Map.h:
-
-/Users/alexduong/Desktop/HSLU/HS_18/INTRO/workspace.kds/KillMe_Remote/Static_Code/IO_Map/MK20D5.h:
--- KillMe_SumoRobot/Debug/Project_Settings/Startup_Code/startup.d
+++
@@ -1,8 +0,0 @@
-Project_Settings/Startup_Code/startup.o: \
- ../Project_Settings/Startup_Code/startup.c \
- /Users/alexduong/Desktop/HSLU/HS_18/INTRO/workspace.kds/Hello_World/Generated_Code/IO_Map.h \
- /Users/alexduong/Desktop/HSLU/HS_18/INTRO/workspace.kds/Hello_World/Static_Code/IO_Map/MK22F12.h
-
-/Users/alexduong/Desktop/HSLU/HS_18/INTRO/workspace.kds/Hello_World/Generated_Code/IO_Map.h:
-
-/Users/alexduong/Desktop/HSLU/HS_18/INTRO/workspace.kds/Hello_World/Static_Code/IO_Map/MK22F12.h:
